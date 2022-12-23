2e6f1a238aa8bfce9848a7f80d4650849261f636
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-12-23 22:33:36 +0100

    Build the home feature in body

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-  searchForm.classList.toggle('active');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
 }
 
 let loginForm = document.querySelector('.login-form-container');
 
-document.querySelector('#login-btn').onclick = () =>{
-  loginForm.classList.toggle('active');
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
 }
 
-document.querySelector('#close-login-btn').onclick = () =>{
-  loginForm.classList.remove('active');
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
 }
 
 window.onscroll = () => {
@@ -32,3 +32,23 @@ window.onload = () => {
     }
     fadeOut();
 }
+
+var swiper = new Swiper(".books-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
