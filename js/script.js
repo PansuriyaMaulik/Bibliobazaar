02cb8fa62eb90fe5c2c2391a17f2ba81d5b2e523
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-12-23 22:47:33 +0100

    Build feature function of navbar sections

--- js/script.js
+++ js/script.js
@@ -14,6 +14,14 @@ document.querySelector('#close-login-btn').onclick = () => {
     loginForm.classList.remove('active');
 }
 
+function loader() {
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut() {
+    setTimeout(loader, 4000);
+}
+
 window.onscroll = () => {
     searchForm.classList.remove('active');
 
@@ -52,3 +60,31 @@ var swiper = new Swiper(".books-slider", {
         },
     },
 });
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
